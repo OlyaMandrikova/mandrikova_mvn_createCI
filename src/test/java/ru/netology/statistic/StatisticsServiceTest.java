591d42d54af52836df14220abac74d5ce62eefdb
Olga Mandrikova <mandrikovaolya@gmail.com>
2025-06-14 23:22:26 +0300

    Reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,9 +1,8 @@
 package ru.netology.statistic;
 
 
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
